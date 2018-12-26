a0ae879fcbd64153b70427689deb4f28d30a03b1
Colin Zarzycki <zarzycki@ucar.edu>
2018-12-25 19:34:14 -0700

    initial cleanup of gen_domain script

--- gen_domain/genUnigridDomains.sh
+++ gen_domain/genUnigridDomains.sh
@@ -31,20 +31,19 @@
 
 # This is the top-level directory where the domain files will be written
 OUTBASE="/glade/work/zarzycki/unigridFiles/"
-#PATHTONCL="/glade/u/home/zarzycki/shellScripts/vrGridGen/"
 
+#atmName will be used in the domain filenames.
+#atmGridName is the path to the scrip file of the VR mesh
 atmName="ne0np4tcfplane.ne15x8"
 atmGridName="/glade/u/home/zarzycki/work/grids/scrip/${atmName}.g_scrip.nc"
 
-# Need to have write access to this directory, so either copy the exec
-# or rebuild 
+# Top level path to CIME mapping tools
+# Note: need to have write access to this directory, so either copy the exec
+# or checkout and build in your own dir
 PATH_TO_MAPPING="/glade/work/zarzycki/cesm2_0_beta08/cime/tools/mapping/"
 
-#----------------------------------------------------------------------
-# Set user-defined parameters here
-#----------------------------------------------------------------------
-cd $PATH_TO_MAPPING/gen_domain_files
-
+# =====================================================================
+# May need to change these settings, but safe to use t12 for mask for anything >ne30
 ocnName="tx0.1v2"
 ocnGridName="/glade/p/cesmdata/cseg/mapping/grids/tx0.1v2_090127.nc"
 wgtFileDir="."
@@ -53,24 +52,25 @@ cdate=`date +%y%m%d`
 
 aaveMap=map_${ocnName}_TO_${atmName}_aave.${cdate}.nc
 
+#----------------------------------------------------------------------
+# CREATE MAPPING FILE
+#----------------------------------------------------------------------
+cd $PATH_TO_MAPPING/gen_domain_files
+
 # do ATM2OCN_FMAPNAME (aave)
 interp_method="conserve"   # bilinear, patch, conserve
-#ncl ${PATHTONCL}/gen_X_to_Y_wgts.ncl 'srcName="'${ocnName}'"' 'srcGridName="'${ocnGridName}'"' 'dstName="'${atmName}'"' 'dstGridName="'${atmGridName}'"' 'wgtFileDir="'${wgtFileDir}'"' 'InterpMethod="'${interp_method}'"'
 ESMF_RegridWeightGen --ignore_unmapped -m ${interp_method} -w ${aaveMap} -s ${ocnGridName} -d ${atmGridName}
 
 #----------------------------------------------------------------------
-# MOVING FILES
+# CREATE DOMAIN FILES
 #----------------------------------------------------------------------
-
 
 set +e
-
-#----------------------------------------------------------------------
-# MOVING ON TO DOMAIN
-#----------------------------------------------------------------------
-
 ./gen_domain -m ${aaveMap} -o tx01 -l ${atmName}
 
+#----------------------------------------------------------------------
+# MOVING FILES + CLEANUP 
+#----------------------------------------------------------------------
 # Move domain files to OUTBASE dir
 mkdir -p ${OUTBASE}/${atmName}/domains
 mv domain*${atmName}*${cdate}*nc ${OUTBASE}/${atmName}/domains
