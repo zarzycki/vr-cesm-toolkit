5d25d63259e2e97afea7654c4ebced2017ddbc35
Colin Zarzycki <zarzycki@ucar.edu>
2024-04-27 06:57:46 -0600

    make batch-grid-gen work on pm and derecho

--- batch-grid-gen.sh
+++ batch-grid-gen.sh
@@ -1,117 +1,189 @@
 #!/bin/bash -l
 
-#INDEX="001"
-INDEX=$1
+#PBS -A P93300642
+#PBS -N batch-gen-vr
+#PBS -q main
+#PBS -j oe
+#PBS -l job_priority=premium
+#PBS -l walltime=7:59:00
+#PBS -l select=1:ncpus=128:mpiprocs=128:ompthreads=1
 
-EXODUSFILE="TClandfall-${INDEX}_ne48x4.g"
+### This echos the line of the script we are on for debugging
+#set -x
 
-echo $EXODUSFILE
+## Special logic for E3SM
+#INDEX=$1
+#EXODUSFILE="TClandfall-${INDEX}_ne48x4.g"
+#atmName="TClandfall-${INDEX}_ne48x4_pg2"
+#atmGridName="/global/homes/c/czarzyck/m2637/E3SM_SCREAM_files/grids/scrip/TClandfall-${INDEX}_ne48x4_pg2_scrip.nc"
+#echo $EXODUSFILE
 
-atmName="TClandfall-${INDEX}_ne48x4_pg2"
-atmGridName="/global/homes/c/czarzyck/m2637/E3SM_SCREAM_files/grids/scrip/TClandfall-${INDEX}_ne48x4_pg2_scrip.nc"
-lndName="ne128pg2"
-lndGridName="/global/homes/c/czarzyck/m2637/E3SM_SCREAM_files/grids/scrip/ne128pg2_scrip.nc"
+###### GRIDS
+EXODUSFILE="ne0np4natlanticwat.ne30x8.g"
+atmName="ne0np4natlanticwat.ne30x8"
+atmGridName="/glade/work/zarzycki/grids/scrip/ne0np4natlanticwat.ne30x8_np4_SCRIP.nc"
+#lndName="ne128pg2"
+#lndGridName="/global/homes/c/czarzyck/m2637/E3SM_SCREAM_files/grids/scrip/ne128pg2_scrip.nc"
 rofName="r0125"
-rofGridName="/global/cfs/cdirs/e3sm/inputdata/lnd/clm2/mappingdata/grids/SCRIPgrid_0.125x0.125_nomask_c170126.nc"
+rofGridName="/glade/campaign/cesm/cesmdata/inputdata/lnd/clm2/mappingdata/grids/SCRIPgrid_0.125x0.125_nomask_c170126.nc"
 
+##### MASKS
+# E3SM
 #maskName="oRRS15to5"
 #maskGridName="/global/cfs/cdirs/e3sm/inputdata/ocn/mpas-o/oRRS15to5/ocean.RRS.15-5km_scrip_151209.nc"
-maskName="ICOS10"
-maskGridName="/global/cfs/cdirs/e3sm/inputdata/ocn/mpas-o/ICOS10/ocean.ICOS10.scrip.211015.nc"
+#maskName="ICOS10"
+#maskGridName="/global/cfs/cdirs/e3sm/inputdata/ocn/mpas-o/ICOS10/ocean.ICOS10.scrip.211015.nc"
+# CESM
+maskName="tx0.1v2"
+maskGridName="/glade/p/cesmdata/cseg/inputdata/share/scripgrids/tx0.1v2_090127.nc"
 
-OUTBASE="/global/cfs/cdirs/m2637/E3SM_SCREAM_files/"
-PATH_TO_MAPPING="/global/homes/c/czarzyck/E3SM-20230714/cime/tools/mapping/"
-SCRATCHDIR="/pscratch/sd/c/czarzyck/"
+do_e3sm_topo=false
+do_cesm_topo=false
+generate_maps=true
+generate_domain=true
+generate_atmsrf=true
+cdate=`date +%y%m%d`
+
+MACHINE="NCAR" # NERSC or NCAR <-- allows for machine-specific settings
+
+# Conditional assignment based on MACHINE
+if [ "$MACHINE" = "NERSC" ]; then
+  OUTBASE="/global/cfs/cdirs/m2637/E3SM_SCREAM_files/"
+  PATH_TO_MAPPING="/global/homes/c/czarzyck/E3SM-20230714/cime/tools/mapping/"
+  SCRATCHDIR="/pscratch/sd/c/czarzyck/"
+elif [ "$MACHINE" = "NCAR" ]; then
+  OUTBASE="/glade/work/zarzycki/CESM_files/"
+  PATH_TO_MAPPING="/glade/work/zarzycki/cesm2_2_1/cime/tools/mapping/"
+  SCRATCHDIR="/glade/derecho/scratch/zarzycki/"
+else
+  echo "Unknown machine type: $MACHINE"
+  exit 1
+fi
+
+if [ "$MACHINE" == "NERSC" ]; then
+  conda activate e3sm_unified_1.8.1_nompi
+fi
+if [ "$MACHINE" == "NCAR" ]; then
+  module load esmf
+fi
 
 # # Extract the path without the filename
 # atmGridPath="${atmGridName%/*}/"
 # # Extract the filename
 # atmGridFilename="${atmGridName##*/}"
 
-cdate=`date +%y%m%d`
+# Check for necessary binaries
+if ! command -v ncl >/dev/null 2>&1; then
+  echo "ncl is not in the PATH. Please install/activate it." ; exit 1
+fi
 
-conda activate e3sm_unified_1.8.1_nompi
+if ! command -v ESMF_RegridWeightGen >/dev/null 2>&1; then
+  echo "ESMF_RegridWeightGen is not in the PATH. Please install/activate it." ; exit 1
+fi
 
 set -e
 
-#---------------------------------------------------------------------------------------------
-
-cd e3sm/
-
-date
-sbatch --wait e3sm-topo.sh \
- $EXODUSFILE \
- $OUTBASE/grids/ \
- $OUTBASE/topo/
-echo $? ; date
-
-cd ..
+mkdir -p $OUTBASE
 
 #---------------------------------------------------------------------------------------------
 
-cd gen_mapping/
-
-date
-(./merged-mapping.sh \
-  --atmName "$atmName" \
-  --atmGridName "$atmGridName" \
-  --lndName "$lndName" \
-  --lndGridName "$lndGridName" \
-  --rofName "$rofName" \
-  --rofGridName "$rofGridName" \
-)
-echo $? ; date
-
-echo "Cleaning up gen_mapping"
-mv -v ${SCRATCHDIR}/tmp.maps.$cdate/map_*.nc /global/cfs/cdirs/m2637/E3SM_SCREAM_files/grids/maps/
-rm -rfv ${SCRATCHDIR}/tmp.maps.$cdate/
-
-cd ..
+if [ "$do_e3sm_topo" == true ]; then
+  cd e3sm/
+  date
+  sbatch --wait e3sm-topo.sh \
+   $EXODUSFILE \
+   $OUTBASE/grids/ \
+   $OUTBASE/topo/
+  echo $? ; date
+  cd ..
+elif [ "$do_cesm_topo" == true ]; then
+  # DO PETER LAURITZEN STUFF HERE
+  echo "Need to put some CESM logic in, chief!"
+fi
 
 #---------------------------------------------------------------------------------------------
 
-cd gen_domain/
+if [ "$generate_maps" == true ]; then
+  cd gen_mapping/
 
-date
-(./genUnigridDomains.sh \
-  $atmName \
-  $atmGridName \
-  $maskName \
-  $maskGridName \
-  $OUTBASE/grids/domains \
-  $PATH_TO_MAPPING \
-)
-echo $? ; date
+  date
+  (./merged-mapping.sh \
+    --atmName "$atmName" \
+    --atmGridName "$atmGridName" \
+    --lndName "$lndName" \
+    --lndGridName "$lndGridName" \
+    --rofName "$rofName" \
+    --rofGridName "$rofGridName" \
+    --wgtFileDir "$SCRATCHDIR" \
+  )
+  echo $? ; date
 
-# If the land and atm are different grids, do this again
-if [ "$atmGridName" != "$lndGridName" ]; then
-  (date
-  ./genUnigridDomains.sh \
-    $lndName \
-    $lndGridName \
+  echo "Cleaning up gen_mapping"
+  mkdir -p ${OUTBASE}/grids/maps/
+  mv -v ${SCRATCHDIR}/tmp.maps.$cdate/map_*.nc ${OUTBASE}/grids/maps/
+  rm -rfv ${SCRATCHDIR}/tmp.maps.$cdate/
+
+  cd ..
+fi
+
+#---------------------------------------------------------------------------------------------
+
+if [ "$generate_domain" == true ]; then
+
+  cd gen_domain/
+
+  date
+  (./genUnigridDomains.sh \
+    $atmName \
+    $atmGridName \
     $maskName \
     $maskGridName \
     $OUTBASE/grids/domains \
-    $PATH_TO_MAPPING
-  echo $? ; date)
+    $PATH_TO_MAPPING \
+    $MACHINE \
+  )
+  echo $? ; date
+
+  # If the land and atm are different grids, and lndGridName exists do this again
+  if [ "$atmGridName" != "$lndGridName" ] && [ -n "$lndGridName" ]; then
+    (date
+    ./genUnigridDomains.sh \
+      $lndName \
+      $lndGridName \
+      $maskName \
+      $maskGridName \
+      $OUTBASE/grids/domains \
+      $PATH_TO_MAPPING \
+      $MACHINE
+    echo $? ; date)
+  else
+    echo "Not doing atm<->land domain"
+  fi
+
+  cd ..
+
 fi
-
-
-cd ..
 
 #---------------------------------------------------------------------------------------------
 
-cd gen_atmsrf/
+if [ "$generate_atmsrf" == true ]; then
 
-set +e
-date
-ncl gen_se_mkatmsrf_from_se.ncl \
-  'dstName="'${atmName}'"' \
-  'dstGridDir="'${atmGridName%/*}/'"' \
-  'dstGridFile="'${atmGridName##*/}'"' \
-  'atmsrfDir="'${OUTBASE}'/atmsrf/"' \
-  'wgtFileDir="'${SCRATCHDIR}'"'
-date
-set -e
+  cd gen_atmsrf/
 
-cd ..
+  ATMSRFOUTLOC="${OUTBASE}/atmsrf/"
+  mkdir -p "$ATMSRFOUTLOC"
+  set +e
+  date
+  ncl gen_se_mkatmsrf_from_se.ncl \
+    'dstName="'${atmName}'"' \
+    'dstGridDir="'${atmGridName%/*}/'"' \
+    'dstGridFile="'${atmGridName##*/}'"' \
+    'atmsrfDir="'${ATMSRFOUTLOC}'"' \
+    'wgtFileDir="'${SCRATCHDIR}'"' \
+    'MACHINE="'${MACHINE}'"'
+  date
+  set -e
+
+  cd ..
+
+fi
